729a250cff0e035af5eed2cff19c986ec620d01a
nugudevelopers <nugudevelopers@users.noreply.github.com>
2022-07-15 00:36:56 +0000

    jekyll build from Action 2bb8976101ad6dd76c1b95893227f3ca823ba66d

---
+++ _assets/base/code.js
@@ -0,0 +1,37 @@
+(($) => {
+  class Code {
+    constructor(options) {
+      this.options = options
+    }
+
+    init() {
+      let copy_codes = $('.code').find('[data-code-copy-target-id]:not([data-potion-handled])')
+
+      copy_codes.attr('data-potion-handled', 'true')
+        .on('click', e => {
+          e.preventDefault()
+
+          let $clicked = $(e.currentTarget)
+
+          parent = $clicked.parents('.code')
+          let code = parent.find('td.rouge-code').text().trim()
+
+          navigator.clipboard.writeText(code).then(() => {
+            let $success = parent.find('.copy-text')
+
+            if ($success[0]) {
+              $success.addClass('show')
+              setTimeout(() => $success.removeClass('show'), 1000)
+            }
+          })
+        })
+
+      return copy_codes
+    }
+  }
+
+  $.code = (options) => {
+    return new Code(options)
+  }
+})(jQuery)
+
---
+++ _assets/base/tabs.js
@@ -0,0 +1,35 @@
+(($) => {
+  class Tabs {
+    constructor(options) {
+      this.options = options
+    }
+
+    init() {
+      let tabNavs = $('.tabs').find('[data-tabs-content-id]:not([data-potion-handled])')
+
+      tabNavs.attr('data-potion-handled', 'true')
+        .on('click', e => {
+          e.preventDefault()
+
+          let $clicked = $(e.currentTarget)
+
+          parent = $clicked.parents('.tabs')
+          parent.find('[data-tabs-content-id]')
+            .removeClass('active')
+            .each((_, nav) => {
+              parent.find('[id=\'' + $(nav).attr('data-tabs-content-id') + '\']').removeClass('active')
+            })
+
+          $clicked.addClass('active')
+          $('#' + $clicked.attr('data-tabs-content-id')).addClass('active')
+        })
+
+      return tabNavs
+    }
+  }
+
+  $.tabs = (options) => {
+    return new Tabs(options)
+  }
+})(jQuery)
+
